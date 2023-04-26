0642d5ebcf6fcbde590ced435002df8dcba06efd
saffarizadeh <kambiz.saffari@marquette.edu>
2023-04-26 16:07:54 -0500

    Created the Customer class.

---
+++ ClassProject/Model/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject.Model
+{
+    public class Customer
+    {
+        private static int autoIncreament;
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Customer()
+        {
+            autoIncreament++;
+            Id = autoIncreament;
+        }
+    }
+}
