83474d28cb2f5131b28df063cebc3088ed5e119d
Kambiz Saffarizadeh <k.saffarizadeh@gmail.com>
2023-04-26 18:04:15 -0500

    Added customer to Program.

--- ClassProject/Program.cs
+++ ClassProject/Program.cs
@@ -1,7 +1,10 @@
-﻿namespace ClassProject
+﻿using ClassProject.Model;
+
+namespace ClassProject
 {
     public class Program
     {
+        private static Customer customer;
         static void Main(string[] args)
         {
         }
