65e457bbc0ffd7d0dfc6c4a92e0f106507921e52
saffarizadeh <kambiz.saffari@marquette.edu>
2023-05-01 16:41:32 -0500

    Updated version.

---
+++ ClassProject/Model/Appointment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject.Model
+{
+    public class Appointment
+    {
+        private static int autoIncreament;
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+
+        public Appointment()
+        {
+            autoIncreament++;
+            Id = autoIncreament;
+        }
+    }
+}
