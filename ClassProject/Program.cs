ab755f789eb7292e9be30b7a1b9dafba21ce2d2c
Kambiz Saffarizadeh <k.saffarizadeh@gmail.com>
2023-11-25 22:12:18 -0600

    Update Program.cs

--- ClassProject/Program.cs
+++ ClassProject/Program.cs
@@ -13,7 +13,7 @@ namespace ClassProject
         static void Main(string[] args)
         {
             Console.WriteLine("Initializing...");
-            Initialize(); // Create and initialize all objects
+            Initialize(); // Create and initialize all objects ...
             Menu();
         }
 
